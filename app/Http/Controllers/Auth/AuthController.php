cf55d6cf8113b533266a11628fd8a19328e76d5e
Jonas C. da Rosa <jonascr86@gmai.com>
2016-05-16 01:26:09 -0300

    Adicionando buidpacks para trabalhar com heroku.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -37,11 +37,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        if(!Auth::check())
-        {
-            $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
-        }
-
+        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
     }
 
     /**
